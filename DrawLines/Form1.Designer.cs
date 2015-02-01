58e447e9d3729284d102909b088b4398cf629fad
Issame Kastite <sinplomo@gmail.com>
2015-02-01 23:43:14 +0000

    first commit

---
+++ DrawLines/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace DrawLines
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Menu = new System.Windows.Forms.Panel();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.txtIncr = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAngle = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLines = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Dessin = new System.Windows.Forms.Panel();
+            this.Menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Menu
+            // 
+            this.Menu.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.Menu.Controls.Add(this.btnSave);
+            this.Menu.Controls.Add(this.btnDraw);
+            this.Menu.Controls.Add(this.txtIncr);
+            this.Menu.Controls.Add(this.label3);
+            this.Menu.Controls.Add(this.txtLength);
+            this.Menu.Controls.Add(this.label4);
+            this.Menu.Controls.Add(this.txtAngle);
+            this.Menu.Controls.Add(this.label2);
+            this.Menu.Controls.Add(this.txtLines);
+            this.Menu.Controls.Add(this.label1);
+            this.Menu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Menu.Location = new System.Drawing.Point(0, 0);
+            this.Menu.Name = "Menu";
+            this.Menu.Size = new System.Drawing.Size(627, 38);
+            this.Menu.TabIndex = 0;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.White;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Location = new System.Drawing.Point(540, 10);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 22);
+            this.btnSave.TabIndex = 9;
+            this.btnSave.Text = "Sauvegarde";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.BackColor = System.Drawing.Color.White;
+            this.btnDraw.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDraw.Location = new System.Drawing.Point(443, 10);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(75, 22);
+            this.btnDraw.TabIndex = 8;
+            this.btnDraw.Text = "Dessiner";
+            this.btnDraw.UseVisualStyleBackColor = false;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // txtIncr
+            // 
+            this.txtIncr.BackColor = System.Drawing.Color.White;
+            this.txtIncr.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtIncr.Location = new System.Drawing.Point(360, 10);
+            this.txtIncr.Name = "txtIncr";
+            this.txtIncr.Size = new System.Drawing.Size(43, 22);
+            this.txtIncr.TabIndex = 7;
+            this.txtIncr.Text = "10";
+            this.txtIncr.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(300, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Increment";
+            // 
+            // txtLength
+            // 
+            this.txtLength.BackColor = System.Drawing.Color.White;
+            this.txtLength.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtLength.Location = new System.Drawing.Point(252, 10);
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(43, 22);
+            this.txtLength.TabIndex = 5;
+            this.txtLength.Text = "500";
+            this.txtLength.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(201, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Longeur";
+            // 
+            // txtAngle
+            // 
+            this.txtAngle.BackColor = System.Drawing.Color.White;
+            this.txtAngle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtAngle.Location = new System.Drawing.Point(150, 10);
+            this.txtAngle.Name = "txtAngle";
+            this.txtAngle.Size = new System.Drawing.Size(43, 22);
+            this.txtAngle.TabIndex = 3;
+            this.txtAngle.Text = "45";
+            this.txtAngle.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(108, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Angle";
+            // 
+            // txtLines
+            // 
+            this.txtLines.BackColor = System.Drawing.Color.White;
+            this.txtLines.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtLines.Location = new System.Drawing.Point(54, 10);
+            this.txtLines.Name = "txtLines";
+            this.txtLines.Size = new System.Drawing.Size(43, 22);
+            this.txtLines.TabIndex = 1;
+            this.txtLines.Text = "10";
+            this.txtLines.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lignes";
+            // 
+            // Dessin
+            // 
+            this.Dessin.BackColor = System.Drawing.Color.White;
+            this.Dessin.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Dessin.Location = new System.Drawing.Point(0, 38);
+            this.Dessin.Name = "Dessin";
+            this.Dessin.Size = new System.Drawing.Size(627, 381);
+            this.Dessin.TabIndex = 1;
+            this.Dessin.Click += new System.EventHandler(this.Dessin_Click);
+            this.Dessin.Paint += new System.Windows.Forms.PaintEventHandler(this.Dessin_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(627, 419);
+            this.Controls.Add(this.Dessin);
+            this.Controls.Add(this.Menu);
+            this.Font = new System.Drawing.Font("Segoe UI Semilight", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Lignes";
+            this.Menu.ResumeLayout(false);
+            this.Menu.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Menu;
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.TextBox txtIncr;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAngle;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLines;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel Dessin;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ DrawLines/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawLines
+{
+    public partial class Form1 : Form
+    {
+        Pen nPen = new Pen(Color.Black);
+        Graphics Canvas = null;
+        Bitmap bmp;
+
+        static int start_x, start_y;
+        static int end_x, end_y;
+
+        static int angle, increment, nline, length;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            start_x = Dessin.Width * 2 / 3;
+            start_y = Dessin.Height * 2 / 3;
+        }
+
+        private void Dessin_Paint(object sender, PaintEventArgs e)
+        {
+            nPen.Width = 1;
+            Canvas = Dessin.CreateGraphics();
+            Canvas.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            for (int i = 0; i < nline; i++)
+            {
+                DrawCanvas();
+            }
+
+
+        }
+
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+            //start_x = Dessin.Width * 2 / 3;
+            //start_y = Dessin.Height * 2 / 3;
+
+            length = int.Parse(txtLength.Text);
+            angle = int.Parse(txtAngle.Text);
+            increment = int.Parse(txtIncr.Text);
+            nline = int.Parse(txtLines.Text);
+
+
+            Dessin.Refresh();
+
+
+
+        }
+
+        private void DrawCanvas()
+        {
+            angle += int.Parse(txtAngle.Text);
+            length += int.Parse(txtIncr.Text);
+
+            end_x = (int)(start_x + length * Math.Cos(angle * Math.PI / 180));
+            end_y = (int)(start_y + length * Math.Sin(angle * Math.PI / 180));
+
+
+            Point[] drawPoints =
+            {
+                new Point(start_x, start_y),
+                new Point(end_x, end_y)
+            };
+
+
+
+            start_x = end_x;
+            start_y = end_y;
+
+            Canvas.DrawLines(nPen, drawPoints);
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(Dessin.Width, Dessin.Height);
+
+            Graphics g = Graphics.FromImage(bmp);
+            Rectangle rect = Dessin.RectangleToScreen(Dessin.ClientRectangle);
+
+            g.CopyFromScreen(rect.Location, Point.Empty, Dessin.Size);
+
+            string rnd = Path.GetRandomFileName();
+
+            bmp.Save(rnd.Substring(0, rnd.LastIndexOf(".")) + ".png");
+
+        }
+
+        private void Dessin_Click(object sender, EventArgs e)
+        {
+            start_x = MousePosition.X;
+            start_y = MousePosition.Y;
+        }
+
+    }
+}
